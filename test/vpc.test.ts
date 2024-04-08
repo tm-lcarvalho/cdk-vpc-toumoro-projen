c8fb5a2b36ce4f771dbf6033cdbc0ce4e9712a6a
tm-lcarvalho <lucio.carvalho@toumoro.com>
2024-04-08 14:27:31 -0400

    test VPC

---
+++ test/vpc.test.ts
@@ -0,0 +1,19 @@
+import { App, Stack } from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { Vpc } from '../src';
+
+describe('CheapVpc', () => {
+  it('Should match snapshot', () => {
+    // Given
+    const app = new App();
+    const stack = new Stack(app, 'Network', { env: { region: 'us-east-1', account: '123456789012' } });
+    new Vpc(stack, 'Vpc');
+
+    // When
+    const template = Template.fromStack(stack);
+
+    // Then
+    expect(template).toMatchSnapshot();
+  });
+
+});
