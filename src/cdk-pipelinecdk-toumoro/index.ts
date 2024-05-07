44bb039d4029b9bd55fd23c37318b114a5f20c89
tm-lcarvalho <lucio.carvalho@toumoro.com>
2024-05-07 10:37:01 -0400

    fix(PipelineCdkDeploy): PipelineCdkDeploy to CDK DeploySimple

--- src/cdk-pipelinecdk-toumoro/index.ts
+++ src/cdk-pipelinecdk-toumoro/index.ts
@@ -1,65 +1,28 @@
-import { CfnOutput } from 'aws-cdk-lib';
-import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
-import * as codepipeline_actions from 'aws-cdk-lib/aws_codepipeline_actions';
+import { CfnOutput, SecretValue } from 'aws-cdk-lib';
+import * as pipelines  from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
+
+
+// https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.pipelines-readme.html
 
 export interface PipelineProps {
   readonly pipelineName: string;
+  readonly repoName: string;
+  readonly repoBranch: string;
 }
 
 export class Pipeline extends Construct {
-  public readonly pipeline: codepipeline.Pipeline;
+  public readonly pipeline: pipelines.CodePipeline;
 
   constructor(scope: Construct, id: string, props: PipelineProps) {
     super(scope, id);
 
     // Create a pipeline
-    this.pipeline = new codepipeline.Pipeline(this, 'Pipeline', {
-      pipelineName: props.pipelineName,
-      stages: [
-        {
-          stageName: 'Source',
-          actions: [
-            new codepipeline_actions.GitHubSourceAction({
-              actionName: 'GitHub_Source',
-              owner: 'tm-lcarvalho',
-              repo: 'cdk-vpc-toumoro-projen',
-              oauthToken: cdk.SecretValue.secretsManager('github-token'),
-              output: new codepipeline.Artifact(),
-            }),
-          ],
-        },
-        {
-          stageName: 'Build',
-          actions: [
-            new codepipeline_actions.CodeBuildAction({
-              actionName: 'CodeBuild',
-              project: new codepipeline_actions.PipelineProject(this, 'CodeBuildProject', {
-                projectName: 'CodeBuildProject',
-                buildSpec: codepipeline_actions.BuildSpec.fromObject({
-                  version: '0.2',
-                  phases: {
-                    install: {
-                      commands: ['npm install'],
-                    },
-                    build: {
-                      commands: ['npm run build'],
-                    },
-                  },
-                }),
-              }),
-              input: new codepipeline.Artifact(),
-              outputs: [new codepipeline.Artifact()],
-            }),
-          ],
-        },
-      ],
-    });
-
-    new CfnOutput(this, 'PipelineName', {
-      value: this.pipeline.pipelineName,
-      description: 'Pipeline Name',
-    });
+    this.pipeline = new pipelines.CodePipeline(this, props.pipelineName, {
+      synth: new pipelines.ShellStep('Synth', {
+        input: pipelines.CodePipelineSource.gitHub(props.repoName, props.repoBranch),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth']
+      }),
 
 
 
