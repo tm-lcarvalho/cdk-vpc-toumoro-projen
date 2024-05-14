4cc34abcec2b12da56628a51f5bf08a2fcd93645
tm-lcarvalho <lucio.carvalho@toumoro.com>
2024-05-14 11:22:45 -0400

    docs(PipelineCdk): add descriptions to parameters

--- src/cdk-pipelinecdk/index.ts
+++ src/cdk-pipelinecdk/index.ts
@@ -7,14 +7,37 @@ import { Construct } from 'constructs';
 
 // Define the properties for the Pipeline construct
 export interface PipelineProps {
+  /**
+   * The name of the pipeline.
+   */
   readonly pipelineName: string;
+
+  /**
+   * The name of the repository.
+   */
   readonly repoName: string;
+
+  /**
+   * The branch of the repository to use.
+   */
   readonly repoBranch: string;
 }
 
+/**
+ * A CDK construct that creates a CodePipeline.
+ */
 export class PipelineCdk extends Construct {
+  /**
+   * The CodePipeline created by the construct.
+   */
   public readonly pipeline: pipelines.CodePipeline;
-
+  /**
+   * Constructs a new instance of the PipelineCdk class.
+   * @param scope The parent construct.
+   * @param id The name of the construct.
+   * @param props The properties for the construct.
+   * @default - No default properties.
+   *  */
   constructor(scope: Construct, id: string, props: PipelineProps) {
     super(scope, id);
 
