d000835e58f1fb187ae776a3a5b8b3c81c4978f1
tm-lcarvalho <lucio.carvalho@toumoro.com>
2024-05-14 10:31:47 -0400

    feat(VpcBase): add gateway endpoints

--- test/vpc.test.ts
+++ test/vpc.test.ts
@@ -8,7 +8,12 @@ test('VPCBase Construct creates a VPC with the specified CIDR block', () => {
 
   // Instantiate the VPC construct with a specific CIDR block
   new VpcBase(stack, 'MyVpc', {
-    cidr: '10.0.0.0/16', // Specify the CIDR block for the VPC
+    cidr: '10.0.0.0/16',
+    maxAzs: 2,
+    natGateways: 2,
+    enableEndpointS3: true,
+    enableEndpointDynamoDB: true,
+    // Specify the CIDR block for the VPC
     // You can pass any other required properties here
   });
 
